1abd70d53549f7d06d00c2203cf82d7b74bd5058
UdemeKen <udemekendrick@gmail.com>
2024-10-18 16:13:28 +0100

    new update

--- src/views/successConfirmations/SuccessfulPayments.jsx
+++ src/views/successConfirmations/SuccessfulPayments.jsx
@@ -7,8 +7,9 @@ export default function SuccessfulPayment() {
     return (
         <section className='flex flex-col justify-center items-center pt-[10rem]'>
             <div className='shadow-md shadow-slate-500 flex flex-col justify-center items-center rounded-lg space-y-8 capitalize p-[5rem]'>
-                <h1 className='text-3xl font-bold text-green-600'>-successful-</h1>
+                <h1 className='text-3xl font-bold text-green-600'>-payment successful-</h1>
                 <p className='text-[16px] font-semibold'>navigate back to dashboard</p>
+                <Link to={"/default/customerdashboard"} className='bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700 transition duration-300'>dashboard</Link>
             </div>
         </section>
     )
