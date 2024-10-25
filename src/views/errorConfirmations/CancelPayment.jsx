cee98c9df27c3e16b55a29f702830401ab71b1de
UdemeKen <udemekendrick@gmail.com>
2024-10-25 07:33:42 +0100

    updated

--- src/views/errorConfirmations/CancelPayment.jsx
+++ src/views/errorConfirmations/CancelPayment.jsx
@@ -9,7 +9,7 @@ export default function CancelPayment() {
             <div className='shadow-md shadow-slate-500 flex flex-col justify-center items-center rounded-lg space-y-8 capitalize p-[5rem]'>
                 <h1 className='text-3xl font-bold text-red-600'>-payment cancelled-</h1>
                 <p className='text-[16px] font-semibold'>navigate back to dashboard</p>
-                <Link to={"/default/customerdashboard"} className='bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700 transition duration-300'>dashboard</Link>
+                <Link to={"https://ibedc-pay-v-2.vercel.app/default/customerdashboard"} className='bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700 transition duration-300'>dashboard</Link>
             </div>
         </section>
     )
